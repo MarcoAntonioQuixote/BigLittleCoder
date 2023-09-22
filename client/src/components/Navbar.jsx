59b9eb8fe20419af0b344a624dfaa944be240a11
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2023-09-22 12:06:05 -0600

    Improved navbar - design and Links applied via RR

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,31 +1,30 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import webHeader from '../assets/bigLittleCoder.png'
 import { AppContext } from '../storeManagement/AppContext';
 
 
 function Navbar() {
 
-    const {app} = useContext(AppContext);
     const links = ['Big Little Coder','Cohorts','1-on-1','Projects','Mark Anthony'];
+    const location = useLocation();
 
     function SpecialLink({link}) {
-
+        let path = decodeURIComponent(location.pathname);
+        path = path === "/" ? '/Big Little Coder' : path;
+        let special = `/${link}` === path ? 'navFocus' : 'navNoFocus';
+        
         let split = [...link];
-        let special = link === app.currentPage ? 'navHover' : null;
-
         const showSplit = split.map((letter,i) =>         
             <span key={i} style={{'--i': i}} >{letter}</span>
         )
 
-        if (link === app.currentPage) {
-
-        }
-
         return (
-            <h2 className={special}>
-                {showSplit}
-            </h2>
+            <Link to={link} className={special}>
+                <h2 >
+                    {showSplit}
+                </h2>
+            </Link>
         )
     }
 
@@ -36,14 +35,6 @@ function Navbar() {
         <div id="navLinks">
             {links.map(link => <SpecialLink key={link} link={link} />)}
         </div>
-
-        {/* <div id='navLinks'>
-            <Link to='/'>Big Little Coder</Link>
-            <Link to='/1-on-1'>1-on-1</Link>
-            <Link to='/cohorts'>Cohort</Link>
-            <Link to='/projects'>Projects</Link>
-            <Link to='/MarkAnthony'>Mark Anthony</Link>
-        </div> */}
     </div>
     )
 }
