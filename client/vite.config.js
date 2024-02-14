869d50fda43c60fe12821d9e82b92ee7b5d66cb5
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2024-02-14 10:41:58 -0600

    Updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Checker from 'vite-plugin-checker'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Checker({typescript: false})],
+  plugins: [
+    react(), 
+    Checker({typescript: false}), 
+    chunkSplitPlugin(
+      {
+        strategy: 'unbundle'
+      }
+    )
+  ],
   server: {
     host: '0.0.0.0',
     port: 3000
-  }
+  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes('node_modules')) {
+  //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 })
