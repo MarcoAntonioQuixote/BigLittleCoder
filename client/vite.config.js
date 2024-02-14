9348025c9775196924db5180268581d0b937d919
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2024-02-14 13:56:45 -0600

    Trying to configure AWS amplify

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,32 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Checker from 'vite-plugin-checker'
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
-    Checker({typescript: false}), 
-    chunkSplitPlugin(
-      {
-        strategy: 'unbundle'
-      }
-    )
+    Checker({typescript: false}),
   ],
   server: {
     host: '0.0.0.0',
     port: 3000
   },
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id) {
-  //         if (id.includes('node_modules')) {
-  //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
 })
